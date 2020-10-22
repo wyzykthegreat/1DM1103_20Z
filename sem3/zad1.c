fcfd320a8664704fb2dafdf91dc08c82071a8b8a
Aleksander Wyżykowski <wyzykjr@gmail.com>
2020-10-22 21:16:39 +0200

    chyba ostatecznie sie udalo

--- sem3/zad1.c
+++ sem3/zad1.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int  argc, char *argv[])
+
+ int main(int argc, char *argv[])
 {
-    char n[];
-    n="Ala";
-    
+    printf("Wartość %.0f do kwadratu to %.0f\n", atof(argv[1]), atof(argv[1])*atof(argv[1]));
     return 0;
 }
+
