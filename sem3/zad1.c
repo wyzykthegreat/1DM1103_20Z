ab3afbf0a5242b6bf0dfe5f4a4827b49e7b4ccce
wyzykthegreat <73235830+wyzykthegreat@users.noreply.github.com>
2020-10-22 23:10:06 +0200

    Update zad1.c

--- sem3/zad1.c
+++ sem3/zad1.c
@@ -4,7 +4,7 @@
 
  int main(int argc, char *argv[])
 {
-    printf("Wartość %.0f do kwadratu to %.0f\n", atof(argv[1]), atof(argv[1])*atof(argv[1]));
+    printf("Wartość %.0f do kwadratu to %.0f.\n", atof(argv[1]), atof(argv[1])*atof(argv[1]));
     return 0;
 }
 
